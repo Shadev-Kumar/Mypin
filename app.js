c562e2fc0a6e0eb3e8bdcaff4e031400b3a7b732
shadevkumar <shadevsrk261@gmail.com>
2023-12-20 02:00:42 +0530

    Initialising app

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use('/css',express.static('dist'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs","./public/**/*.javascripts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
