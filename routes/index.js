27c04436d4061bcbb6e3b27c2057ed23182234df
shadevkumar <shadevsrk261@gmail.com>
2023-12-21 12:00:38 +0530

    Create Pin , feed, profile improved

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,73 @@
 var express = require('express')
 var router = express.Router()
 let UserModel = require('./users')
+let postsModel = require('./posts')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const upload = require('./multer')
 
 passport.use(new LocalStrategy(UserModel.authenticate()))
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index')
+  res.render('index', { nav: false })
 })
 
 router.get('/register', function (req, res, next) {
-  res.render('register')
+  res.render('register', { nav: false })
 })
 
-router.get('/profile', isLoggedIn, async function (req, res,next) {
+router.get('/profile', isLoggedIn, async function (req, res, next) {
+  const user = await UserModel.findOne({
+    username: req.session.passport.user,
+  }).populate('posts')
+  res.render('profile', { user, nav: true })
+})
+
+router.get('/create', isLoggedIn, async function (req, res, next) {
   const user = await UserModel.findOne({ username: req.session.passport.user })
-  res.render('profile', { user })
+  res.render('create', { user, nav: true })
 })
 
-router.post('/fileupload', isLoggedIn, upload.single("image"), async function (req, res, next) {
+router.get('/showposts', isLoggedIn, async function (req, res, next) {
+  const user = await UserModel.findOne({
+    username: req.session.passport.user,
+  }).populate('posts')
+  res.render('showposts', { user, nav: true })
+})
+
+router.get('/feed', isLoggedIn, async function (req, res, next) {
+  const user = await UserModel.findOne({ username: req.session.passport.user })
+  const posts = await postsModel.find().populate('user')
+
+  res.render('feed', { user,posts, nav: true })
+})
+
+router.post(
+  '/createpost',
+  isLoggedIn,
+  upload.single('postimage'),
+  async function (req, res, next) {
+    const user = await UserModel.findOne({
+      username: req.session.passport.user,
+    })
+    const post = await postsModel.create({
+      user: user._id,
+      title: req.body.title,
+      description: req.body.description,
+      image: req.file.filename,
+    })
+
+    user.posts.push(post._id)
+    await user.save()
+    res.redirect('/profile')
+  },
+)
+
+router.post('/fileupload', isLoggedIn, upload.single('image'), async function (
+  req,
+  res,
+  next,
+) {
   const user = await UserModel.findOne({ username: req.session.passport.user })
   user.profileImage = req.file.filename
   await user.save()
@@ -31,6 +77,7 @@ router.post('/fileupload', isLoggedIn, upload.single("image"), async function (r
 router.post('/register', function (req, res) {
   var userdata = new UserModel({
     username: req.body.username,
+    name: req.body.name,
     email: req.body.secret,
   })
   UserModel.register(userdata, req.body.password).then(function (
--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,15 @@ const userschema = mongoose.Schema({
   password: String,
   email: String,
   profileImage: String,
-  contact:Number,
+  // contact:Number,
   boards:{
     type:Array,
     default:[]
-  }
+  },
+  posts:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"post"
+  }],
 });
 
 userschema.plugin(plm)
